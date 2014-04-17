d72608e8e85e02a5ff56010216cbd6b0727ead37
Levi Tan Ong <levi@byimplication.com>
2014-04-17 15:18:18 +0800

    table of agencies

--- public/javascripts/app/admin/admin.view.js
+++ public/javascripts/app/admin/admin.view.js
@@ -1,4 +1,18 @@
 admin.view = function(ctrl){
+  var agencyList = m.prop([
+    {
+      shortname: "OCD",
+      name: "Office of Civil Defense",
+      permissions: "xxrrdd",
+      userCount: 42, 
+    },
+    {
+      shortname: "DPWH",
+      name: "Department of Public Works and Highways",
+      permissions: "xxrrdd",
+      userCount: 145, 
+    },
+  ])
   return app.template(ctrl, [
     common.banner("Administrative Interface"),
     m("section", [
@@ -22,7 +36,34 @@ admin.view = function(ctrl){
                   m("td", [
                     "Agency Name"
                   ]),
+                  m("td", [
+                    "Acronym"
+                  ]),
+                  m("td", [
+                    "Users"
+                  ]),
+                  m("td", [
+                    "Permissions"
+                  ]),
                 ]),
+              ]),
+              m("tbody", [
+                agencyList().map(function(a){
+                  return m("tr", [
+                    m("td", [
+                      a.name
+                    ]),
+                    m("td", [
+                      a.shortname
+                    ]),
+                    m("td", [
+                      a.userCount
+                    ]),
+                    m("td", [
+                      a.permissions
+                    ]),
+                  ]);
+                })
               ]),
             ]),
           ]),
