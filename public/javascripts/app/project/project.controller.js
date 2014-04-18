3904b55f98ee5d4aa579d86d0b45d4f90dfeb262
Levi Tan Ong <levi@byimplication.com>
2014-04-19 07:00:48 +0800

    adds stringify, applies new template to dropzone

--- public/javascripts/app/project/project.controller.js
+++ public/javascripts/app/project/project.controller.js
@@ -8,14 +8,37 @@ project.controller = function(){
   database.pull().then(function(data){
     this.project(database.projectList()[this.id - 1]);
   }.bind(this))
-  
+
   this.initMap = function(elem, isInit){
     this.app.initMap(elem, isInit, {scrollWheelZoom: false});
   }.bind(this)
 
   this.initImageDropzone = function(elem, isInit){
+    var pt = m(".dz-preview.dz-file-preview", [
+      m(".dz-details", [
+        m(".dz-filename", [
+          m("span", {"data-dz-name": true}, "hi"),
+        ]),
+        m(".dz-size", {"data-dz-size": true}),
+        m("img", {"data-dz-thumbnail": true}),
+      ]),
+      m(".dz-progress", [
+        m("span.dz-upload", {"data-dz-uploadprogress": true}),
+      ]),
+      m(".dz-success-mark", [
+        m("i.fa.fa-check"),
+      ]),
+      m(".dz-error-mark", [
+        m("i.fa.fa-times")
+      ]),
+      m(".dz-error-message", [
+        m("span", {"data-dz-errormessage": true})
+      ]),
+    ]);
+
     if(!isInit){
-      var dropzone = new Dropzone(elem, {url: "/file/post"})
+      var dropzone = new Dropzone(elem, {url: "/file/post", previewTemplate: m.stringify(pt)})
+
     }
   }
 
