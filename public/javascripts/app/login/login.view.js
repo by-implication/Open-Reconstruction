73a9dd5ecfb6981e212050b24a0858502dc4c51f
Levi Tan Ong <levi@byimplication.com>
2014-04-18 21:22:03 +0800

    styles login page

--- public/javascripts/app/login/login.view.js
+++ public/javascripts/app/login/login.view.js
@@ -1,9 +1,16 @@
 login.view = function(ctrl){
-  return app.template(ctrl, [
+  return app.template(ctrl, {className: "detail"}, [
+    common.banner("Open Reconstruction"),
     m("section", [
       m(".row", [
         m(".columns.medium-6.medium-centered", [
           m(".card", [
+            m(".section", [
+              m("h2", [
+                "Log in"
+              ]),
+            ]),
+            m("hr"),
             m(".section", [
               m("form", [
                 m("label", [
