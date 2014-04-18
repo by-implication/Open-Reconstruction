57d2d1d2335f9daec40390f12d78e0ea3fdcbec0
Levi Tan Ong <levi@byimplication.com>
2014-04-18 19:00:09 +0800

    adds inputs for login

--- public/javascripts/app/login/login.view.js
+++ public/javascripts/app/login/login.view.js
@@ -1,7 +1,25 @@
 login.view = function(ctrl){
   return app.template(ctrl, [
     m("section", [
-      "hi"
+      m(".row", [
+        m(".columns.medium-6.medium-centered", [
+          m(".card", [
+            m(".section", [
+              m("form", [
+                m("label", [
+                  "Username",
+                  m("input[type='text']"),
+                ]),
+                m("label", [
+                  "Password",
+                  m("input[type='password']")
+                ]),
+                m("button", "Log in")
+              ]),
+            ]),
+          ]),
+        ]),
+      ]),
     ]),
   ])
 }
