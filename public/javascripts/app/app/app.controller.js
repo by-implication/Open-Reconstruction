cc236033c90ada8acd282a8db7345946e9087501
Levi Tan Ong <levi@byimplication.com>
2014-04-19 16:15:48 +0800

    removes old sampledata functions in app

--- public/javascripts/app/app/app.controller.js
+++ public/javascripts/app/app/app.controller.js
@@ -20,35 +20,6 @@ app.controller = function(){
     return this.isSuperAdmin() || this.currentUser() && this.currentUser().isAdmin && this.currentUser().agency.id === agencyId;
   }
   
-  this.isLoggedIn = function(){
-    var currentUserId = localStorage["currentUser"];
-    if(currentUserId && this.findUserBySlug(database.userList(), currentUserId)){
-      return true;
-    } else {
-      return false;
-    }
-  }.bind(this);
-  this.login = function(user){
-    localStorage["currentUser"] = user.slug
-  };
-  this.logout = function(){
-    // this.currentUser(new user.GUEST());
-    localStorage["currentUser"] = null;
-  };
-  this.getLoggedIn = function(){
-    var currentUserId = localStorage["currentUser"];
-    return this.findUserBySlug(database.userList(), currentUserId);
-  }.bind(this);
-  this.findUserBySlug = function(list, slug){
-    return _.find(list, function(u){
-      return u.slug == slug;
-    });
-  };
-  this.authorizedUsers = function(){
-    return database.userList().filter(function(user){
-      return user.department === "OCD" || user.department === "DPWH";
-    });
-  };
   this.initMap = function(elem, isInit, config){
     if(!isInit){
       window.setTimeout(function(){
