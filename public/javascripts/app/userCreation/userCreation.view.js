798fd2bc194cffb4984e44d2d2622b732ce05022
Levi Tan Ong <levi@byimplication.com>
2014-04-19 13:06:36 +0800

    styles for user creation

--- public/javascripts/app/userCreation/userCreation.view.js
+++ public/javascripts/app/userCreation/userCreation.view.js
@@ -1,22 +1,26 @@
 userCreation.view = function(ctrl){
   return app.template(ctrl.app, [
-    common.banner("User Creation"),
+    common.banner("Adding a user for " + ctrl.agency().name),
     m("form", {onsubmit: ctrl.submit}, [
       common.formSection(
-        "fa-star",
+        "fa-user",
         [
-          m("div", "for " + ctrl.agency().name),
           common.field(
             "Username",
-            m("input[type='text']", {onchange: m.withAttr("value", ctrl.input.handle)})
+            m("input[type='text']", {onchange: m.withAttr("value", ctrl.input.handle)}),
+            "In general the pattern should be [first initial][middle initial][surname]. For example, Juan Carlos Dizon de Guzman should be jddeguzman."
           ),
           common.field(
             "Password",
             m("input[type='password']", {onchange: m.withAttr("value", ctrl.input.password)})
           ),
           common.field(
-            "Admin",
-            m("input[type='checkbox']", {onchange: m.withAttr("value", ctrl.input.isAdmin)},"")
+            "Privileges",
+            m("label", [
+              m("input[type='checkbox']", {onchange: m.withAttr("value", ctrl.input.isAdmin)},""),
+              m("span", "Make this user an admin for this agency")
+            ]),
+            "As an admin, this user will be able to add and delete users"
           )
         ]
       ),
