16921b5a6ecbb9b595b60d207ae62a563ae059fb
Pepe Bawagan <syk0saje@gmail.com>
2014-04-19 21:51:04 +0800

    windows psql is picky

--- resetdb.sh
+++ resetdb.sh
@@ -5,5 +5,5 @@ else
 	DATABASE=$1
 fi
 echo $DATABASE
-psql -Upostgres $DATABASE -c"drop schema public cascade"
-psql -Upostgres $DATABASE -c"create schema public"
+psql -Upostgres -c"drop schema public cascade" $DATABASE
+psql -Upostgres -c"create schema public" $DATABASE
