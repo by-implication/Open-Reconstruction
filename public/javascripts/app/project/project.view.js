9ef00a183e816d42df48d77fba535db0b11ba5da
Albert Dizon <akdizon@gmail.com>
2014-04-20 17:15:57 +0800

    workify assigned requests

--- public/javascripts/app/project/project.view.js
+++ public/javascripts/app/project/project.view.js
@@ -217,6 +217,9 @@ project.listView = function(ctrl){
           return p.projectType == ctrl.currentFilter.projects();
         }
       })
+      .filter(function(p){
+        return (p.canSignoff || ctrl.tabs.currentTab() == "All")
+      })
       .sortBy(function(p){
         return p.date;
       })
