44819f91a3e698f2232ce50c954f560a569c095f
Albert Dizon <akdizon@gmail.com>
2014-04-21 06:09:38 +0800

    show user's project in user view

--- public/javascripts/app/user/user.controller.js
+++ public/javascripts/app/user/user.controller.js
@@ -6,9 +6,13 @@ user.controller = function(){
   this.projectList = m.prop([]);
   this.currentFilter = {projects: function(){return null}};
 
+  this.tabs = new common.tabs.controller();
+  this.tabs.tabs = m.prop([]);
+
   m.request({method: "GET", url: ("/users/" + self.id + "/meta"), config: app.xhrConfig}).then(function (r){
     if(r.success){
       this.user(r.user)
+      this.projectList = r.requests;
     } else {
       alert(r.reason);
     }
