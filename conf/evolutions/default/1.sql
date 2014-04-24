26d25c4a5b212abb70e3a8cbfc7e629660e7c41c
unknown <pepe@byimplication.com>
2014-04-24 18:26:46 +0800

    reorganize evolutions

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,15 +1,42 @@
 # --- !Ups
 
-CREATE TYPE disaster_type AS ENUM(
-	'Typhoon',
-    'Earthquake',
-    'Flood',
-	'Landslide',
-	'Fire',
-	'Anthropogenic',
-    'None or Preventive'
+CREATE EXTENSION IF NOT EXISTS pgcrypto;;
+
+CREATE TABLE roles (
+    role_id serial PRIMARY KEY,
+    role_name text NOT NULL,
+    role_permissions int[] NOT NULL
+);;
+
+CREATE TABLE agencys (
+    agency_id serial PRIMARY KEY,
+    agency_name text NOT NULL,
+    agency_acronym text,
+    role_id int NOT NULL REFERENCES roles
+);;
+
+CREATE TABLE municipalitys (
+    municipality_id serial PRIMARY KEY REFERENCES agencys(agency_id),
+    municipality_nthClass int NOT NULL
+);;
+
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    user_handle text NOT NULL,
+    user_name text NOT NULL,
+    user_password text NOT NULL,
+    agency_id int NOT NULL REFERENCES agencys,
+    user_admin boolean NOT NULL DEFAULT false
 );;
 
 # --- !Downs
 
-DROP TYPE IF EXISTS disaster_type;;
+DROP TABLE IF EXISTS users;;
+
+DROP TABLE IF EXISTS municipalitys;;
+
+DROP TABLE IF EXISTS agencys;;
+
+DROP TABLE IF EXISTS roles;;
+
+DROP EXTENSION IF EXISTS pgcrypto;;
