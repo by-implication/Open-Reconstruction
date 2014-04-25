a4cb670b343dea3b201bb00e2d258b7231434d73
Philip Cheang <phi@byimplication.com>
2014-04-26 05:15:35 +0800

    reverting changes

--- public/javascripts/app/app/app.view.js
+++ public/javascripts/app/app/app.view.js
@@ -30,7 +30,7 @@ app.navbar = function(ctrl){
     m("ul.title-area", [
       m("li.name", [
         m("h1", [
-          m("a", {href: "/projects", config: m.route}, "Open Reconstruction - plzwork")
+          m("a", {href: "/projects", config: m.route}, "Open Reconstruction")
         ])
       ])
     ]),
