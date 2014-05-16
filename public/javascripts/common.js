d425cf97cdf590403f3329f726e14a228c3bf746
syk0saje <syk0saje@gmail.com>
2014-05-16 17:41:20 +0800

    fix tab behavior to work with new m.route

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -215,7 +215,7 @@ common.tabs.content = function(ctrl){
 common.tabs.controller = function(basePath){
   this.tabs = m.prop([]);
   this.currentTab = function() {
-    var item = _.find(this.tabs(), function(tab) { return tab.href == m.route.path });
+    var item = _.find(this.tabs(), function(tab) { return tab.href == m.route() });
     if(item == undefined) {
       item = _.head(this.tabs());
     }
