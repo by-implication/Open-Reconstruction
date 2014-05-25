9e8646ef4bb99c313edd9467232738f3de0ba1b6
Levi Tan Ong <levi@byimplication.com>
2014-05-25 22:10:58 +0800

    uncommits yolanda data

--- public/javascripts/app/dashboard/controller.js
+++ public/javascripts/app/dashboard/controller.js
@@ -7,25 +7,25 @@ dashboard.controller = function(){
     self.requests(r);
   });
 
-  // bi.ajax(routes.controllers.Assets.at("data/yolanda.json")).then(function (r){
-  //   console.log("Yolanda Data");
-  //   var data = r.values.map(function(e){
-  //     var obj = _.object(r.headers, e);
-  //     if(!_.isUndefined(obj.Amount)){
-  //       obj.Amount = Number.parseFloat(obj.Amount.replace(/\,/g, ""));
-  //     }
-  //     if(!_.isUndefined(obj.Date)){
-  //       var dateArr = obj.Date.split("/")
-  //       if(dateArr.length === 3){
-  //         obj.Date = new Date(Date.parse(dateArr[1] + "/" + dateArr[0] + "/" + dateArr[2]));
-  //       } else {
-  //         console.log("tang ina lang. " + obj.Date);
-  //       }
-  //     }
-  //     return obj;
-  //   })
-  //   console.log(data);
-  // });
+  bi.ajax(routes.controllers.Assets.at("data/yolanda.json")).then(function (r){
+    console.log("Yolanda Data");
+    var data = r.values.map(function(e){
+      var obj = _.object(r.headers, e);
+      if(!_.isUndefined(obj.Amount)){
+        obj.Amount = Number.parseFloat(obj.Amount.replace(/\,/g, ""));
+      }
+      if(!_.isUndefined(obj.Date)){
+        var dateArr = obj.Date.split("/")
+        if(dateArr.length === 3){
+          obj.Date = new Date(Date.parse(dateArr[1] + "/" + dateArr[0] + "/" + dateArr[2]));
+        } else {
+          console.log("tang ina lang. " + obj.Date);
+        }
+      }
+      return obj;
+    })
+    console.log(data);
+  });
 
   this.pendingProjects = function(){
     return this.totalProjects() - this.approvedProjects().length;
