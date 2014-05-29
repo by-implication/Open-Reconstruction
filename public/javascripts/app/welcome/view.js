42161aefc796a3d05c7bbab49bc876d6c9e21085
Philip Cheang <phi@byimplication.com>
2014-05-29 16:19:33 +0800

    Move big number stats to welcome. (Preparing for panel merge)

--- public/javascripts/app/welcome/view.js
+++ public/javascripts/app/welcome/view.js
@@ -14,7 +14,7 @@ welcome.view = function(ctrl){
           ]),
           m("div", {class: "columns medium-4 info"}, [
             m("h2", "Info 1"),
-            m("h2", "Info 2")            
+            m("h2", "Info 2")
           ])
         ])
       ]),
@@ -27,6 +27,69 @@ welcome.view = function(ctrl){
             ])
           ])
         ]),
+      ]),
+m("section.proposals", [
+        m(".row", [
+          m(".columns.medium-12", [
+            m("h1", [m("small", "Project Proposals")]),
+          ]),
+          m(".columns.medium-3", [
+            m("i.fa.fa-file-text-o.fa-5x"),
+            m(".val-group", [
+              m("h2", ctrl.byLevel()[0].count),
+              m(".caption", "Proposals Submitted"),
+            ]),
+            m(".val-group", [
+              m("h2", helper.truncate(ctrl.byLevel()[0].amount)),
+              m(".caption", "Total Cost (PHP)")
+            ]),
+          ]),
+          m(".columns.medium-3", [
+            m("i.fa.fa-search.fa-5x"),
+            m(".val-group", [
+              m("h2", [ctrl.byLevel()[1].count+ " ",
+                m("span", "(n%)")
+              ]),
+              m("span", "Proposals for Assessment"),
+            ]),
+            m(".val-group", [
+              m("h2", helper.truncate(
+                parseFloat(ctrl.byLevel()[1].amount) +
+                parseFloat(ctrl.byLevel()[2].amount)
+              )),
+              m("span", "Cost of pending projects (PHP)")
+            ]),
+          ]),
+          m(".columns.medium-3", [
+            m("i.fa.fa-check-circle-o.fa-5x"),
+            m(".val-group", [
+              m("h2", [ctrl.byLevel()[3].count+ " ",
+                m("span", "(n%)")
+              ]),
+              m("span", "Proposals for Approval"),
+            ]),
+            m(".val-group", [
+              m("h2", helper.truncate(
+                parseFloat(ctrl.byLevel()[4].amount) +
+                parseFloat(ctrl.byLevel()[5].amount)
+              )),
+              m("span", "Total Approved Cost")
+            ]),
+          ]),
+          m(".columns.medium-3", [
+            m("i.fa.fa-money.fa-5x"),
+            m(".val-group", [
+              m("h2", [ctrl.byLevel()[4].count + " ",
+                m("span", "(" + helper.percent(ctrl.percentApproved()) + ")")
+              ]),
+              m("span", "Projects for funding"),
+            ]),
+            m(".val-group", [
+              m("h2", ctrl.byLevel()[5].amount),
+              m("span", "Amount for disbursal")
+            ]),
+          ])
+        ])
       ]),
       m("section.alt", [
         m(".row", [
