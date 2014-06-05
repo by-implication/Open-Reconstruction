fcc6d87e838e9c96f0039583505541ff57328575
syk0saje <syk0saje@gmail.com>
2014-06-05 15:50:44 +0800

    make projects cascade with reqs, add psql isnumeric function

--- conf/evolutions/default/5.sql
+++ conf/evolutions/default/5.sql
@@ -1,5 +1,15 @@
 # --- !Ups
 
+CREATE OR REPLACE FUNCTION isnumeric(text) RETURNS BOOLEAN AS $$
+DECLARE x NUMERIC;;
+BEGIN
+    x = $1::NUMERIC;;
+    RETURN TRUE;;
+EXCEPTION WHEN others THEN
+    RETURN FALSE;;
+END;;
+$$ LANGUAGE plpgsql IMMUTABLE;;
 
 # --- !Downs
 
+DROP FUNCTION IF EXISTS isnumeric(text);;
