8584eb87e14363f8463f4c88f086ad4ec6d837f4
syk0saje <syk0saje@gmail.com>
2014-06-10 05:28:46 +0800

    make select2 update onchange on rerender

--- public/javascripts/app/select2/controller.js
+++ public/javascripts/app/select2/controller.js
@@ -1,14 +1,15 @@
 select2.config = function(ctrl){
   return function(elem, isInit){
     var el = $(elem);
-    if (!isInit) {
-      el.select2()
-        .on("change", function(e){
-          m.startComputation();
-          if (typeof ctrl.onchange === "function") ctrl.onchange(el.select2("val"));
-          m.endComputation();
-        });
-    };
+    el.select2().off("change")
+      .on("change", function(e){
+        m.startComputation();
+        if (typeof ctrl.onchange === "function"){
+          console.log('triggered')
+          ctrl.onchange(el.select2("val"));
+        }
+        m.endComputation();
+      });
     el.select2("val", ctrl.value);
   }
 }
