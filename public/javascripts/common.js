b37a106a56f14a8487d661dc0f13a0b49a7a0fa6
Levi Tan Ong <levi@byimplication.com>
2014-06-16 17:44:14 +0800

    more efficient algo for sticky

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -345,6 +345,8 @@ common.stickyTabs.config = function(ctrl){
 
 common.sticky = {};
 common.sticky.config = function(ctrl){
+  // ctrl.cachedMaxScrollRange = 0;
+
   return function(elem, isInit){
     var maxScrollRange = function(){
       var parent = $(elem).parent();
@@ -358,7 +360,7 @@ common.sticky.config = function(ctrl){
       var scrollTop = $(window).scrollTop()
       var top = 0;
       if (scrollTop > initialTop(elem)) {
-        top = Math.min(scrollTop, maxScrollRange()) - initialTop(elem);
+        top = Math.min(scrollTop, ctrl.cachedMaxScrollRange) - initialTop(elem);
       }
       $(elem).css("top", top);
     }
@@ -367,6 +369,11 @@ common.sticky.config = function(ctrl){
       $(elem).css("position", "relative")
     };
     if(!isInit){
+      if (_.isUndefined(ctrl.cachedMaxScrollRange)) {
+        setTimeout(function(){
+          ctrl.cachedMaxScrollRange = maxScrollRange();
+        }, 500)
+      };
       $(window).on("scroll", function(e){
         adjustLayout();
       });
