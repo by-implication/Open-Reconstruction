ae44772bf97f8601990c20d232005cc77bfb3e41
Levi Tan Ong <levi@byimplication.com>
2014-07-03 13:00:27 +0800

    prelim styling

--- public/javascripts/app/requestEdit/view.js
+++ public/javascripts/app/requestEdit/view.js
@@ -5,20 +5,26 @@ requestEdit.view = function(ctrl){
     "Edit Legacy Request — " + ctrl.request().description,
     {className: "detail"},
     [
-    	m("h1", "Editing Legacy Request"),
-    	m("form", {onsubmit: ctrl.submit}, [
-	    	common.field("Status",
-	    		select2.view({data: process.levelDict.map(function (l, i){
-	    			return {id: i, name: l};
-	    		}), value: ctrl.status(), onchange: ctrl.status})
-	  		),
-	    	common.dateField("Date Received", ctrl.date, ctrl.htmlDate),
-	    	common.field("SARO No.",
-	    		m("input", {value: ctrl.saroNo(), onchange: m.withAttr("value", ctrl.saroNo)})
-	  		),
-	  		m("button", "Submit"),
-	  		m("a.alert.button", {href: routes.controllers.Requests.view(ctrl.id).url}, "Cancel")
-  		])
+      m("section", [
+        m(".row", [
+          m(".columns.medium-12", [
+            m("h1", "Editing Legacy Request"),
+            m("form", {onsubmit: ctrl.submit}, [
+              common.field("Status",
+                select2.view({data: process.levelDict.map(function (l, i){
+                  return {id: i, name: l};
+                }), value: ctrl.status(), onchange: ctrl.status})
+              ),
+              common.dateField("Date Received", ctrl.date, ctrl.htmlDate),
+              common.field("SARO No.",
+                m("input", {value: ctrl.saroNo(), type: "text", onchange: m.withAttr("value", ctrl.saroNo)})
+              ),
+              m("button", "Submit"),
+              m("a.alert.button", {href: routes.controllers.Requests.view(ctrl.id).url}, "Cancel")
+            ])
+          ]),
+        ]),
+      ]),
     ]
   );
 
