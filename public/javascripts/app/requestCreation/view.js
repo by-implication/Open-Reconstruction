813bb9ec0f4b9a152c138557e93068d95108ed38
Levi Tan Ong <levi@byimplication.com>
2014-07-07 20:15:47 +0800

    moves everything into a card

--- public/javascripts/app/requestCreation/view.js
+++ public/javascripts/app/requestCreation/view.js
@@ -106,132 +106,122 @@ requestCreation.view = function(ctrl){
         }
       )
     ],
-    m("form", {onsubmit: ctrl.submitNewRequest }, [
-      m("section", [
-        m(".row", [
-          m(".columns.medium-12", [
-            m("h2", ["Terms of Agreement"]),
-            common.field(
-              "",
-              m("div", [
-                m("input", {type: "checkbox", onchange: m.withAttr("checked", ctrl.preamble)}),
-                m("span", [
-                  "I have not requested for assistance for this project from any other source."
-                ])
-              ])
-            )
-          ]),
-        ]),
-      ]),
-      ctrl.preamble() ? m("div", [
-        m("section", [
-          m(".row", [
-            m(".columns.medium-12", [
+    m(".row", [
+      m(".columns.large-12", [
+        m(".card", [
+          m("form", {onsubmit: ctrl.submitNewRequest }, [
+            m(".section", [
+              m("h2", ["Terms of Agreement"]),
               common.field(
-                "Disaster",
-                m("select", {onchange: m.withAttr("value", ctrl.disasterId)},
-                  ctrl.info().disasters.map(function (d){
-                    return m("option", {value: d.id}, d.name);
-                  })
-                )
-              ),
+                "",
+                m("div", [
+                  m("input", {type: "checkbox", onchange: m.withAttr("checked", ctrl.preamble)}),
+                  m("span", [
+                    "I have not requested for assistance for this project from any other source."
+                  ])
+                ])
+              )
             ]),
-          ]),
-        ]),
-        m("section.alt", [
-          m(".row", [
-            m(".columns.medium-12", [
-              m("h2", [
-                "Specific Requests"
+            ctrl.preamble() ? m("div", [
+              m(".section", [
+                common.field(
+                  "Disaster",
+                  m("select", {onchange: m.withAttr("value", ctrl.disasterId)},
+                    ctrl.info().disasters.map(function (d){
+                      return m("option", {value: d.id}, d.name);
+                    })
+                  )
+                ),
               ]),
-              m("p.help", [
-                "Each of these requests will get their own page, with their own statuses and tracking."
-              ]),
-              m("table", [
-                m("thead", [
-                  m("tr", [
-                    m("th", [
-                      "Description"
-                    ]),
-                    m("th", [
-                      "Estimated Amount"
-                    ]),
-                    m("th", [
-                      "Type"
-                    ]),
-                    m("th", [
-                      "Location"
-                    ]),
-                    m("th", [
-                      "Attachments"
-                    ]),
-                    m("th", [
-                      "Edit"
+              m(".section", [
+                m("h2", [
+                  "Specific Requests"
+                ]),
+                m("p.help", [
+                  "Each of these requests will get their own page, with their own statuses and tracking."
+                ]),
+                m("table", [
+                  m("thead", [
+                    m("tr", [
+                      m("th", [
+                        "Description"
+                      ]),
+                      m("th", [
+                        "Estimated Amount"
+                      ]),
+                      m("th", [
+                        "Type"
+                      ]),
+                      m("th", [
+                        "Location"
+                      ]),
+                      m("th", [
+                        "Attachments"
+                      ]),
+                      m("th", [
+                        "Edit"
+                      ]),
                     ]),
                   ]),
+                  m("tbody", ctrl.entries.map(function (e){
+                    return m("tr", [
+                      m("td", [
+                        m("input", {onchange: m.withAttr("value", e.description), type: "text", placeholder: "e.g. Reconstruction of a seawall for barangay A"}),
+                      ]),
+                      m("td", [
+                        m("input", {type: "number", onchange: m.withAttr("value", e.amount)}),
+                      ]),
+                      m("td", [
+                        m("select", {
+                          onchange: m.withAttr("value", e.projectTypeId),
+                          value: e.projectTypeId()
+                        }, [
+                          m("optgroup", {label: "Infrastructure"}, [
+                            projectTypeGroups([0, 10])
+                          ]),
+                          m("optgroup", {label: "Water"}, [
+                            projectTypeGroups([5, 7, 8, 9, 12, 13])
+                          ]),
+                          m("optgroup", {label: "Buildings"}, [
+                            projectTypeGroups([1, 3, 6, 11])
+                          ]),
+                          m("optgroup", {label: "Other"}, [
+                            projectTypeGroups([2, 4, 14])
+                          ]),
+                        ])
+                      ]),
+                      m("td", [
+                        m("td", m("button[type=button]", {onclick: e.openLocationModal}, "Set location"))
+                      ]),
+                      m("td", [
+                        m("td", m("button[type=button]", {onclick: e.openAttachmentsModal}, "Add attachments"))
+                      ]),
+                      m("td", m("button.alert[type=button]", {onclick: e.remove}, "Baleeted!"))
+                    ])
+                  })),
                 ]),
-                m("tbody", ctrl.entries.map(function (e){
-                  return m("tr", [
-                    m("td", [
-                      m("input", {onchange: m.withAttr("value", e.description), type: "text", placeholder: "e.g. Reconstruction of a seawall for barangay A"}),
-                    ]),
-                    m("td", [
-                      m("input", {type: "number", onchange: m.withAttr("value", e.amount)}),
-                    ]),
-                    m("td", [
-                      m("select", {
-                        onchange: m.withAttr("value", e.projectTypeId),
-                        value: e.projectTypeId()
-                      }, [
-                        m("optgroup", {label: "Infrastructure"}, [
-                          projectTypeGroups([0, 10])
-                        ]),
-                        m("optgroup", {label: "Water"}, [
-                          projectTypeGroups([5, 7, 8, 9, 12, 13])
-                        ]),
-                        m("optgroup", {label: "Buildings"}, [
-                          projectTypeGroups([1, 3, 6, 11])
-                        ]),
-                        m("optgroup", {label: "Other"}, [
-                          projectTypeGroups([2, 4, 14])
-                        ]),
-                      ])
-                    ]),
-                    m("td", [
-                      m("td", m("button[type=button]", {onclick: e.openLocationModal}, "Set location"))
-                    ]),
-                    m("td", [
-                      m("td", m("button[type=button]", {onclick: e.openAttachmentsModal}, "Add attachments"))
-                    ]),
-                    m("td", m("button.alert[type=button]", {onclick: e.remove}, "Baleeted!"))
-                  ])
-                })),
+                m("button", {type: "button", onclick: ctrl.newEntry}, [
+                  "Add new entry"
+                ]),
               ]),
-              m("button", {type: "button", onclick: ctrl.newEntry}, [
-                "Add new entry"
+              m(".section", [
+                m("ul.button-group", [
+                  m("li", [
+                    m("button", {disabled: ctrl.submitButtonDisabled(), onclick: function(e){
+                      ctrl.submitButtonDisabled(true);
+                      ctrl.submitNewRequest(e);
+                    }}, "Submit"),
+                  ]),
+                  m("li", [
+                    m("button", {type: "button", class: "alert", onclick: cancel}, "Cancel"),
+                  ]),
+                ]),
               ]),
-            ]),
-          ]),
+            ])
+            : ""
+          ])
         ]),
-        m("section", [
-          m(".row", [
-            m(".columns.medium-12", [
-              m("ul.button-group", [
-                m("li", [
-                  m("button", {disabled: ctrl.submitButtonDisabled(), onclick: function(e){
-                    ctrl.submitButtonDisabled(true);
-                    ctrl.submitNewRequest(e);
-                  }}, "Submit"),
-                ]),
-                m("li", [
-                  m("button", {type: "button", class: "alert", onclick: cancel}, "Cancel"),
-                ]),
-              ]),
-            ]),
-          ]),
-        ]),
-      ])
-      : ""
-    ])
+      ]),
+    ]),
   ])
 }
