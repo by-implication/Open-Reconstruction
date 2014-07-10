90cce64c2de21a65bd5e830ee2437df9724a632e
syk0saje <syk0saje@gmail.com>
2014-07-10 09:06:34 +0800

    creates path-comprehensive search keys for lgus

--- app/models/GovUnit.scala
+++ app/models/GovUnit.scala
@@ -11,7 +11,7 @@ import recon.support._
 object GovUnit extends GovUnitGen {
 
   def search(s: String): Seq[GovUnit] = DB.withConnection { implicit c =>
-    SQL("SELECT * FROM gov_units WHERE gov_unit_search_key ILIKE {searchKey}")
+    SQL("SELECT * FROM gov_units WHERE gov_unit_search_key ILIKE {searchKey} LIMIT 5")
     .on('searchKey -> ("%" + s.split(" ").mkString("%") + "%")).list(simple)
   }
 
