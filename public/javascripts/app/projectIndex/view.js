5a6dfad9c8c6305539f708ab89c2122ea2d6faea
Levi Tan Ong <levi@byimplication.com>
2014-07-14 20:55:26 +0800

    initial styling for projectIndex

--- public/javascripts/app/projectIndex/view.js
+++ public/javascripts/app/projectIndex/view.js
@@ -10,49 +10,57 @@ projectIndex.view = function(ctrl){
 	);
 
   return app.template(ctrl.app, "Projects", [
-  	m("h1", "Projects"),
-  	pagination,
-  	m("table", [
-      m("thead", [
-        m("tr", [
-          m("td", [
-            "Id"
+    common.banner("Projects for implementation"),
+    m("section", [
+      m(".row", [
+        m(".columns.medium-12", [
+          m("p.help", [
+            "These are entries from agency project management systems (PMS). These are in a separate list from the request list because this comes from a different data source. Requests contain references to these projects."
           ]),
-          m("td", [
-            "Name"
+          pagination,
+          m("table.responsive", [
+            m("thead", [
+              m("tr", [
+                m("td", [
+                  "Id"
+                ]),
+                m("td", [
+                  "Name"
+                ]),
+                m("td", [
+                  "Scope"
+                ]),
+                m("td", [
+                  "Amount"
+                ])
+              ]),
+            ]),
+            m("tbody",
+              ctrl.projects().map(function (p){
+                return m("tr", [
+                  m("td", [
+                    m("a", {href: routes.controllers.Projects.view(p.id).url}, [
+                      p.id
+                    ])
+                  ]),
+                  m("td", [
+                    m("a", {href: routes.controllers.Projects.view(p.id).url}, [
+                      p.name
+                    ])
+                  ]),
+                  m("td", [
+                    p.scope
+                  ]),
+                  m("td", [
+                    helper.commaize(p.amount)
+                  ])
+                ])
+              })
+            ),
           ]),
-          m("td", [
-            "Scope"
-          ]),
-          m("td", [
-            "Amount"
-          ])
+          pagination
         ]),
       ]),
-      m("tbody",
-        ctrl.projects().map(function (p){
-          return m("tr", [
-            m("td", [
-              m("a", {href: routes.controllers.Projects.view(p.id).url}, [
-                p.id
-              ])
-            ]),
-            m("td", [
-              m("a", {href: routes.controllers.Projects.view(p.id).url}, [
-                p.name
-              ])
-            ]),
-            m("td", [
-              p.scope
-            ]),
-            m("td", [
-              helper.commaize(p.amount)
-            ])
-          ])
-        })
-      ),
     ]),
-    pagination
   ]);
-
 }
