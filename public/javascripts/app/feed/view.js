6aa8da49fc04374607884991daad5c2ae20e8e1d
Levi Tan Ong <levi@byimplication.com>
2014-07-17 14:47:45 +0800

    pagination positioning

--- public/javascripts/app/feed/view.js
+++ public/javascripts/app/feed/view.js
@@ -15,21 +15,18 @@ feed.view = function(ctrl){
     {className: "detail"},
     [
       common.banner("Feed"),
-      pagination,
       m("section", [
         m(".row", [
           m(".columns.medium-6.medium-centered", [
-            m(".card",
-              ctrl.events().map(function (e){
-                e.isNew = e.date > ctrl.lastVisit();
-                return feedEvent[e.kind](e);
-              })
-            )
+            pagination,
+            m(".card", ctrl.events().map(function (e){
+              e.isNew = e.date > ctrl.lastVisit();
+              return feedEvent[e.kind](e);
+            })),
+            pagination,
           ]),
         ]),
       ]),
-      pagination
     ]
   )
-
 }
