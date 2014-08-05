9fd43f84890aaf327c79c2d82c0b776cb5623c95
Albert Dizon <akdizon@gmail.com>
2014-08-05 19:13:01 +0800

    submit array of entries

--- public/javascripts/app/userCreation/controller.js
+++ public/javascripts/app/userCreation/controller.js
@@ -2,26 +2,18 @@ userCreation.controller = function(){
   this.app = new app.controller();
   this.slug = m.prop(m.route.param("id"));
   this.govUnit = m.prop({});
-  this.input = {
-    name: m.prop(""),
-    handle: m.prop(""),
-    password: m.prop(""),
-    govUnitId: m.prop(0),
-    isAdmin: m.prop(false)
-  }
 
   var ctrl = this;
 
   bi.ajax(routes.controllers.GovUnits.viewMeta(this.slug(), "users", 1)).then(function (r){
     this.govUnit(r.govUnit);
-    this.input.govUnitId(r.govUnit.id);
   }.bind(this));
 
   this.entries = [];
 
   this.submit = function(e){
     e.preventDefault();
-    bi.ajax(routes.controllers.Users.insert(this.slug()), {data: this.input}).then(function (r){
+    bi.ajax(routes.controllers.Users.insert(this.slug()), {data: this.entries}).then(function (r){
       m.route(routes.controllers.GovUnits.view(this.slug()).url);
     }.bind(this), common.formErrorHandler);
   }.bind(this)
